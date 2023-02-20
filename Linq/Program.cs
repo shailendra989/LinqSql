a748547a17cc85c983e6d395795502fceaa5cdd2
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-02-20 22:41:04 +0530

    saja saja

---
+++ Linq/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Data Table demo");
+            LinqToDataTable linqToDataTable = new LinqToDataTable();
+            linqToDataTable.AddToDataTableDemo();
+            Console.ReadKey();
+        }
+    }
+}
